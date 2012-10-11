327a728117243559ac790f77123ff587f2718997
Kevin Szkudlapski <szkudl.k@gmail.com>
2012-10-11 22:58:07 +0200

    core: start to implement expressions

--- inc/medusa/expression.hpp
+++ inc/medusa/expression.hpp
@@ -1,4 +1,79 @@
 #ifndef _MEDUSA_EXPRESSION_HPP_
 #define _MEDUSA_EXPRESSION_HPP_
 
+#include "medusa/namespace.hpp"
+#include "medusa/export.hpp"
+#include "medusa/types.hpp"
+#include "medusa/address.hpp"
+
+MEDUSA_NAMESPACE_BEGIN
+
+class Expression
+{
+public:
+  virtual std::string ToString(void) const = 0;
+
+private:
+};
+
+class OperationExpression : public Expression
+{
+public:
+  enum Type
+  {
+    OpUnk,
+    OpAff,
+    OpAnd,
+    OpOr,
+    OpXor,
+    OpAdd,
+    OpSub,
+    OpMul,
+    OpDiv
+  };
+  virtual std::string ToString(void) const;
+
+private:
+  u8          m_OpType;
+  Expression *m_pLeftExpr;
+  Expression *m_pRightExpr;
+};
+
+class ConstantExpression : public Expression
+{
+public:
+  virtual std::string ToString(void) const;
+
+private:
+  u32 m_ConstType;
+  u64 m_Value;
+};
+
+class IdentifierExpression : public Expression
+{
+public:
+  virtual std::string ToString(void) const;
+
+private:
+  u32 m_Id;
+};
+
+class MemoryExpression : public Expression
+{
+public:
+  enum Type
+  {
+    OpUnk,
+    OpRead,
+    OpWrite
+  };
+  virtual std::string ToString(void) const;
+
+private:
+  u8 m_MemOpType;
+  Address m_Address;
+};
+
+MEDUSA_NAMESPACE_END
+
 #endif // !_MEDUSA_EXPRESSION_HPP_
--- src/core/expression.cpp
+++ src/core/expression.cpp
@@ -1,1 +1,42 @@
-#include "medusa/export.hpp"
+#include "medusa/expression.hpp"
+#include <sstream>
+#include <boost/format.hpp>
+
+MEDUSA_NAMESPACE_USE
+
+std::string OperationExpression::ToString(void) const
+{
+  static const char *s_StrOp[] = { "???", "=", "&", "|", "^", "+", "-", "*", "/" };
+
+  if (m_pLeftExpr == nullptr || m_pRightExpr == nullptr || m_OpType >= (sizeof(s_StrOp) / sizeof(*s_StrOp)))
+    return "";
+
+  auto LeftStr  = m_pLeftExpr->ToString();
+  auto RightStr = m_pRightExpr->ToString();
+
+  if (LeftStr.empty() || RightStr.empty())
+    return "";
+
+  return (boost::format("%1% %2% %3%") % LeftStr % s_StrOp[m_OpType] % RightStr).str();
+}
+
+std::string ConstantExpression::ToString(void) const
+{
+  // TODO: Handle m_ConstType
+  return (boost::format("%#x") % m_Value).str();
+}
+
+std::string IdentifierExpression::ToString(void) const
+{
+  return (boost::format("Id(%d)") % m_Id).str();
+}
+
+std::string MemoryExpression::ToString(void) const
+{
+  switch (m_MemOpType)
+  {
+  case OpRead:  return (boost::format("Read(%s)")  % m_Address.ToString()).str();
+  case OpWrite: return (boost::format("Write(%s)") % m_Address.ToString()).str();
+  default:      return "";
+  }
+}
