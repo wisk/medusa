66c88869ab11ac74e269c29dbae2f7737d8a1d17
Kevin Szkudlapski <szkudl.k@gmail.com>
2012-11-10 15:41:34 +0100

    core: add view and printer files, use boost::mutex instead of std::mutex

---
+++ inc/medusa/view.hpp
@@ -0,0 +1,75 @@
+#ifndef __MEDUSA_VIEW_HPP__
+#define __MEDUSA_VIEW_HPP__
+
+#include "medusa/namespace.hpp"
+#include "medusa/export.hpp"
+#include "medusa/types.hpp"
+#include "medusa/address.hpp"
+
+#include <vector>
+#include <boost/thread/mutex.hpp>
+
+MEDUSA_NAMESPACE_BEGIN
+
+class Medusa_EXPORT View
+{
+public:
+  class Medusa_EXPORT LineInformation
+  {
+  public:
+    enum Type
+    {
+      UnknownLineType,
+      CellLineType,
+      MultiCellLineType,
+      LabelLineType,
+      XrefLineType,
+      MemoryAreaLineType,
+      EmptyLineType
+    };
+
+    typedef std::vector<LineInformation> Vector;
+
+    LineInformation(Type Type = UnknownLineType, Address const& rAddr = Address())
+      : m_Type(Type)
+      , m_Address(rAddr)
+    {}
+
+    bool operator<(LineInformation const & li) const
+    {
+      if (m_Address < li.m_Address)       return true;
+      else if (m_Address == li.m_Address) return m_Type > li.m_Type;
+      else                                return false;
+    }
+
+    bool operator==(LineInformation const & li) const
+    {
+      return m_Type == li.m_Type && m_Address == li.m_Address;
+    }
+
+    Type GetType(void) const { return m_Type; }
+    Address const& GetAddress(void) const { return m_Address; }
+
+  private:
+    Type     m_Type;
+    Address  m_Address;
+  };
+
+public:
+  void AddLineInformation(LineInformation const & rLineInfo);
+  void EraseLineInformation(LineInformation const & rLineInfo);
+  void UpdateLineInformation(LineInformation const & rLineInfo);
+  bool GetLineInformation(int Line, LineInformation & rLineInfo) const;
+  bool ConvertLineInformationToLine(LineInformation const& rLineInfo, int & rLine) const;
+  size_t GetNumberOfLine(void) const;
+  void EraseAll(void);
+
+private:
+  LineInformation::Vector m_LinesInformation;
+  typedef boost::mutex MutexType;
+  mutable MutexType m_EventMutex;
+};
+
+MEDUSA_NAMESPACE_END
+
+#endif // !__MEDUSA_VIEW_HPP__
---
+++ src/core/view.cpp
@@ -0,0 +1,72 @@
+#include "medusa/view.hpp"
+
+#include <boost/thread/mutex.hpp>
+#include <boost/thread/locks.hpp>
+#include <boost/thread.hpp>
+
+MEDUSA_NAMESPACE_USE;
+
+void View::AddLineInformation(View::LineInformation const & rLineInfo)
+{
+  boost::recursive_mutex::scoped_lock(m_EventMutex);
+  auto itPrevLineInfo = std::lower_bound(std::begin(m_LinesInformation), std::end(m_LinesInformation), rLineInfo);
+  if (itPrevLineInfo == m_LinesInformation.end() || rLineInfo < *itPrevLineInfo)
+    m_LinesInformation.insert(itPrevLineInfo, rLineInfo);
+}
+
+void View::EraseLineInformation(LineInformation const & rLineInfo)
+{
+  boost::recursive_mutex::scoped_lock(m_EventMutex);
+  auto itLineInfo = std::lower_bound(std::begin(m_LinesInformation), std::end(m_LinesInformation), rLineInfo);
+
+  if (itLineInfo == std::end(m_LinesInformation) || rLineInfo < *itLineInfo) return;
+
+  m_LinesInformation.erase(itLineInfo);
+}
+
+void View::UpdateLineInformation(View::LineInformation const & rLineInfo)
+{
+  boost::recursive_mutex::scoped_lock(m_EventMutex);
+  auto itLineInfo = std::lower_bound(std::begin(m_LinesInformation), std::end(m_LinesInformation), rLineInfo);
+
+  if (itLineInfo == std::end(m_LinesInformation) || rLineInfo < *itLineInfo)
+  {
+    AddLineInformation(rLineInfo);
+    return;
+  }
+
+  *itLineInfo = rLineInfo;
+}
+
+bool View::GetLineInformation(int Line, View::LineInformation & rLineInfo) const
+{
+  boost::recursive_mutex::scoped_lock(m_EventMutex);
+  if (Line >= m_LinesInformation.size())
+    return false;
+
+  rLineInfo = m_LinesInformation[Line];
+  return true;
+}
+
+bool View::ConvertLineInformationToLine(View::LineInformation const& rLineInfo, int & rLine) const
+{
+  boost::recursive_mutex::scoped_lock(m_EventMutex);
+  auto itLineInfo = std::lower_bound(std::begin(m_LinesInformation), std::end(m_LinesInformation), rLineInfo);
+
+  if (itLineInfo == std::end(m_LinesInformation) || rLineInfo < *itLineInfo) return false;
+
+  rLine = static_cast<int>(std::distance(std::begin(m_LinesInformation), itLineInfo));
+  return true;
+}
+
+size_t View::GetNumberOfLine(void) const
+{
+  boost::recursive_mutex::scoped_lock(m_EventMutex);
+  return m_LinesInformation.size();
+}
+
+void View::EraseAll(void)
+{
+  boost::recursive_mutex::scoped_lock(m_EventMutex);
+  m_LinesInformation.erase(std::begin(m_LinesInformation), std::end(m_LinesInformation));
+}
