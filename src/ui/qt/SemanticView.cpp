12393896e717563ac449ee150d003e15240bbe7f
Kévin Szkudłapski <szkudl.k@gmail.com>
2013-11-26 23:23:28 +0100

    ui_qt: implement semantic view

--- src/ui/qt/SemanticView.cpp
+++ src/ui/qt/SemanticView.cpp
@@ -4,4 +4,37 @@ SemanticView::SemanticView(QWidget *parent, medusa::Medusa const& core, medusa::
   : QTextEdit(parent)
 {
   setReadOnly(true);
+
+  auto const& doc = core.GetDocument();
+
+  auto func = dynamic_cast<medusa::Function const*>(doc.GetMultiCell(funcAddr));
+  if (func == nullptr)
+    return;
+
+  medusa::ControlFlowGraph cfg;
+  if (!core.BuildControlFlowGraph(funcAddr, cfg))
+    return;
+
+  cfg.ForEachInstruction([&](medusa::Address const& addr)
+  {
+    QString addrStr = QString::fromStdString(addr.ToString()) + ": ";
+    auto insn = std::dynamic_pointer_cast<medusa::Instruction const>(core.GetCell(addr));
+    if (insn == nullptr)
+    {
+      append(addrStr + "(not an instruction)\n");
+      return;
+    }
+
+    auto const& semList = insn->GetSemantic();
+    if (semList.empty())
+    {
+      append(addrStr + "(no semantic available)\n");
+      return;
+    }
+
+    for (auto sem = std::begin(semList); sem != std::end(semList); ++sem)
+    {
+      append(addrStr + QString::fromStdString((*sem)->ToString()));
+    }
+  });
 }
