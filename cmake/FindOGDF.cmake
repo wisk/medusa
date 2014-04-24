85ea49285c67c6f2bc3a0732216e3271bdc0a9d4
wisk <szkudl.k@gmail.com>
2014-04-24 16:45:59 +0200

    cmake: improve FindOGDF script

--- cmake/FindOGDF.cmake
+++ cmake/FindOGDF.cmake
@@ -6,22 +6,26 @@ set(OGDF_INCLUDE_DIRS ${OGDF_PATH}/include)
 
 find_library(COIN_LIBRARY_RELEASE
   NAMES coin COIN
-  PATHS ${OGDF_PATH} ${OGDF_PATH}/Win32/Release ${OGDF_PATH}/x64/Release ${OGDF_PATH}/_release
+  PATHS ${OGDF_PATH}
+  PATH_SUFFIXES build build/Release Release Win32/Release x64/Release _release
   )
 
 find_library(OGDF_LIBRARY_RELEASE
   NAMES ogdf OGDF
-  PATHS ${OGDF_PATH} ${OGDF_PATH}/Win32/Release ${OGDF_PATH}/x64/Release ${OGDF_PATH}/_release
+  PATHS ${OGDF_PATH}
+  PATH_SUFFIXES build build/Release Release Win32/Release x64/Release _release
   )
 
 find_library(COIN_LIBRARY_DEBUG
   NAMES coin COIN
-  PATHS ${OGDF_PATH} ${OGDF_PATH}/Win32/Debug ${OGDF_PATH}/x64/Debug ${OGDF_PATH}/_debug
+  PATHS ${OGDF_PATH}
+  PATH_SUFFIXES build build/Debug Debug Win32/Debug x64/Debug _debug
   )
 
 find_library(OGDF_LIBRARY_DEBUG
   NAMES ogdf OGDF
-  PATHS ${OGDF_PATH} ${OGDF_PATH}/Win32/Debug ${OGDF_PATH}/x64/Debug ${OGDF_PATH}/_debug
+  PATHS ${OGDF_PATH}
+  PATH_SUFFIXES build build/Debug Debug Win32/Debug x64/Debug _debug
   )
 
 set(OGDF_LIBRARIES optimized ${OGDF_LIBRARY_RELEASE} optimized ${COIN_LIBRARY_RELEASE} debug ${OGDF_LIBRARY_DEBUG} debug ${COIN_LIBRARY_DEBUG})
