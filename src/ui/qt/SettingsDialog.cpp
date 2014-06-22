e4554311aa7d399ff5cb68d9ecc06906bd053a6b
Kévin Szkudłapski <szkudl.k@gmail.com>
2014-06-22 22:17:38 +0200

    ui_qt: start to implement keyboard shortcut

--- src/ui/qt/SettingsDialog.cpp
+++ src/ui/qt/SettingsDialog.cpp
@@ -4,29 +4,36 @@
 
 #include <QColorDialog>
 #include <QFontDialog>
+#include <QKeySequenceEdit>
 
 SettingsDialog::SettingsDialog(QWidget * parent)
   : QDialog(parent)
 {
   this->setupUi(this);
 
-  this->connect(this->buttonBox,         SIGNAL(accepted()),         SLOT(saveSettings()));
-  this->connect(this->buttonBox,         SIGNAL(rejected()),         SLOT(close()       ));
+  this->connect(this->ButtonBox,         SIGNAL(accepted()),         SLOT(saveSettings()));
+  this->connect(this->ButtonBox,         SIGNAL(rejected()),         SLOT(close()       ));
 
-  this->connect(this->medusaBackground,  SIGNAL(selectionChanged()), SLOT(setColor()    ));
-  this->connect(this->medusaBackground,  SIGNAL(selectionChanged()), SLOT(setColor()    ));
-  this->connect(this->mnemonicColor,     SIGNAL(selectionChanged()), SLOT(setColor()    ));
-  this->connect(this->registerColor,     SIGNAL(selectionChanged()), SLOT(setColor()    ));
-  this->connect(this->immediateColor,    SIGNAL(selectionChanged()), SLOT(setColor()    ));
-  this->connect(this->commentColor,      SIGNAL(selectionChanged()), SLOT(setColor()    ));
-  this->connect(this->selectionColor,    SIGNAL(selectionChanged()), SLOT(setColor()    ));
-  this->connect(this->operatorColor,     SIGNAL(selectionChanged()), SLOT(setColor()    ));
-  this->connect(this->keywordColor,      SIGNAL(selectionChanged()), SLOT(setColor()    ));
-  this->connect(this->labelColor,        SIGNAL(selectionChanged()), SLOT(setColor()    ));
-  this->connect(this->stringColor,       SIGNAL(selectionChanged()), SLOT(setColor()    ));
-  this->connect(this->addressBackground, SIGNAL(selectionChanged()), SLOT(setColor()    ));
+  this->connect(this->BackgroundColor,   SIGNAL(selectionChanged()), SLOT(setColor()    ));
+  this->connect(this->BackgroundColor,   SIGNAL(selectionChanged()), SLOT(setColor()    ));
+  this->connect(this->MnemonicColor,     SIGNAL(selectionChanged()), SLOT(setColor()    ));
+  this->connect(this->RegisterColor,     SIGNAL(selectionChanged()), SLOT(setColor()    ));
+  this->connect(this->ImmediateColor,    SIGNAL(selectionChanged()), SLOT(setColor()    ));
+  this->connect(this->CommentColor,      SIGNAL(selectionChanged()), SLOT(setColor()    ));
+  this->connect(this->SelectionColor,    SIGNAL(selectionChanged()), SLOT(setColor()    ));
+  this->connect(this->OperatorColor,     SIGNAL(selectionChanged()), SLOT(setColor()    ));
+  this->connect(this->KeywordColor,      SIGNAL(selectionChanged()), SLOT(setColor()    ));
+  this->connect(this->LabelColor,        SIGNAL(selectionChanged()), SLOT(setColor()    ));
+  this->connect(this->StringColor,       SIGNAL(selectionChanged()), SLOT(setColor()    ));
+  this->connect(this->AddressBackground, SIGNAL(selectionChanged()), SLOT(setColor()    ));
 
-  this->connect(this->textFont,          SIGNAL(selectionChanged()), SLOT(setFont()     ));
+  this->connect(this->TextFont,          SIGNAL(selectionChanged()), SLOT(setFont()     ));
+
+  auto pShortcutItem = new QTreeWidgetItem(ShortcutWidget);
+  pShortcutItem->setText(0, "test");
+  pShortcutItem->setText(1, "blabla");
+  auto pKeySeqEdit = new QKeySequenceEdit(this);
+  ShortcutWidget->setItemWidget(pShortcutItem, 2, pKeySeqEdit);
 }
 
 SettingsDialog::~SettingsDialog()
@@ -48,41 +55,41 @@ void    SettingsDialog::showEvent(QShowEvent * e)
   };
 
   // Color
-  setColor(this->medusaBackground,  Settings::instance().value(MEDUSA_COLOR_VIEW_BACKGROUND, MEDUSA_COLOR_VIEW_BACKGROUND_DEFAULT).toString());
-  setColor(this->mnemonicColor,     Settings::instance().value(MEDUSA_COLOR_INSTRUCTION_MNEMONIC, MEDUSA_COLOR_INSTRUCTION_MNEMONIC_DEFAULT).toString());
-  setColor(this->registerColor,     Settings::instance().value(MEDUSA_COLOR_INSTRUCTION_REGISTER, MEDUSA_COLOR_INSTRUCTION_REGISTER_DEFAULT).toString());
-  setColor(this->immediateColor,    Settings::instance().value(MEDUSA_COLOR_INSTRUCTION_IMMEDIATE, MEDUSA_COLOR_INSTRUCTION_IMMEDIATE_DEFAULT).toString());
-  setColor(this->commentColor,      Settings::instance().value(MEDUSA_COLOR_INSTRUCTION_COMMENT, MEDUSA_COLOR_INSTRUCTION_COMMENT_DEFAULT).toString());
-  setColor(this->selectionColor,    Settings::instance().value(MEDUSA_COLOR_INSTRUCTION_SELECTION, MEDUSA_COLOR_INSTRUCTION_SELECTION_DEFAULT).toString());
-  setColor(this->operatorColor,     Settings::instance().value(MEDUSA_COLOR_INSTRUCTION_OPERATOR, MEDUSA_COLOR_INSTRUCTION_OPERATOR_DEFAULT).toString());
-  setColor(this->keywordColor,      Settings::instance().value(MEDUSA_COLOR_INSTRUCTION_KEYWORD, MEDUSA_COLOR_INSTRUCTION_KEYWORD_DEFAULT).toString());
-  setColor(this->labelColor,        Settings::instance().value(MEDUSA_COLOR_INSTRUCTION_LABEL, MEDUSA_COLOR_INSTRUCTION_LABEL_DEFAULT).toString());
-  setColor(this->stringColor,       Settings::instance().value(MEDUSA_COLOR_INSTRUCTION_STRING, MEDUSA_COLOR_INSTRUCTION_STRING_DEFAULT).toString());
-  setColor(this->addressBackground, Settings::instance().value(MEDUSA_COLOR_ADDRESS_BACKGROUND, MEDUSA_COLOR_ADDRESS_BACKGROUND_DEFAULT).toString());
+  setColor(this->BackgroundColor,   Settings::instance().value(MEDUSA_COLOR_VIEW_BACKGROUND, MEDUSA_COLOR_VIEW_BACKGROUND_DEFAULT).toString());
+  setColor(this->MnemonicColor,     Settings::instance().value(MEDUSA_COLOR_INSTRUCTION_MNEMONIC, MEDUSA_COLOR_INSTRUCTION_MNEMONIC_DEFAULT).toString());
+  setColor(this->RegisterColor,     Settings::instance().value(MEDUSA_COLOR_INSTRUCTION_REGISTER, MEDUSA_COLOR_INSTRUCTION_REGISTER_DEFAULT).toString());
+  setColor(this->ImmediateColor,    Settings::instance().value(MEDUSA_COLOR_INSTRUCTION_IMMEDIATE, MEDUSA_COLOR_INSTRUCTION_IMMEDIATE_DEFAULT).toString());
+  setColor(this->CommentColor,      Settings::instance().value(MEDUSA_COLOR_INSTRUCTION_COMMENT, MEDUSA_COLOR_INSTRUCTION_COMMENT_DEFAULT).toString());
+  setColor(this->SelectionColor,    Settings::instance().value(MEDUSA_COLOR_INSTRUCTION_SELECTION, MEDUSA_COLOR_INSTRUCTION_SELECTION_DEFAULT).toString());
+  setColor(this->OperatorColor,     Settings::instance().value(MEDUSA_COLOR_INSTRUCTION_OPERATOR, MEDUSA_COLOR_INSTRUCTION_OPERATOR_DEFAULT).toString());
+  setColor(this->KeywordColor,      Settings::instance().value(MEDUSA_COLOR_INSTRUCTION_KEYWORD, MEDUSA_COLOR_INSTRUCTION_KEYWORD_DEFAULT).toString());
+  setColor(this->LabelColor,        Settings::instance().value(MEDUSA_COLOR_INSTRUCTION_LABEL, MEDUSA_COLOR_INSTRUCTION_LABEL_DEFAULT).toString());
+  setColor(this->StringColor,       Settings::instance().value(MEDUSA_COLOR_INSTRUCTION_STRING, MEDUSA_COLOR_INSTRUCTION_STRING_DEFAULT).toString());
+  setColor(this->AddressBackground, Settings::instance().value(MEDUSA_COLOR_ADDRESS_BACKGROUND, MEDUSA_COLOR_ADDRESS_BACKGROUND_DEFAULT).toString());
 
   // Font
   QFont  font;
   font.fromString(Settings::instance().value(MEDUSA_FONT_TEXT, MEDUSA_FONT_TEXT_DEFAULT).toString());
-  this->textFont->setFont(font);
+  this->TextFont->setFont(font);
 }
 
 void    SettingsDialog::saveSettings()
 {
   // Color
-  Settings::instance().setValue(MEDUSA_COLOR_VIEW_BACKGROUND,       this->medusaBackground->text());
-  Settings::instance().setValue(MEDUSA_COLOR_INSTRUCTION_MNEMONIC,  this->mnemonicColor->text());
-  Settings::instance().setValue(MEDUSA_COLOR_INSTRUCTION_REGISTER,  this->registerColor->text());
-  Settings::instance().setValue(MEDUSA_COLOR_INSTRUCTION_IMMEDIATE, this->immediateColor->text());
-  Settings::instance().setValue(MEDUSA_COLOR_INSTRUCTION_COMMENT,   this->commentColor->text());
-  Settings::instance().setValue(MEDUSA_COLOR_INSTRUCTION_SELECTION, this->selectionColor->text());
-  Settings::instance().setValue(MEDUSA_COLOR_INSTRUCTION_OPERATOR,  this->operatorColor->text());
-  Settings::instance().setValue(MEDUSA_COLOR_INSTRUCTION_KEYWORD,   this->keywordColor->text());
-  Settings::instance().setValue(MEDUSA_COLOR_INSTRUCTION_LABEL,     this->labelColor->text());
-  Settings::instance().setValue(MEDUSA_COLOR_INSTRUCTION_STRING,    this->stringColor->text());
-  Settings::instance().setValue(MEDUSA_COLOR_ADDRESS_BACKGROUND,    this->addressBackground->text());
+  Settings::instance().setValue(MEDUSA_COLOR_VIEW_BACKGROUND,       this->BackgroundColor->text());
+  Settings::instance().setValue(MEDUSA_COLOR_INSTRUCTION_MNEMONIC,  this->MnemonicColor->text());
+  Settings::instance().setValue(MEDUSA_COLOR_INSTRUCTION_REGISTER,  this->RegisterColor->text());
+  Settings::instance().setValue(MEDUSA_COLOR_INSTRUCTION_IMMEDIATE, this->ImmediateColor->text());
+  Settings::instance().setValue(MEDUSA_COLOR_INSTRUCTION_COMMENT,   this->CommentColor->text());
+  Settings::instance().setValue(MEDUSA_COLOR_INSTRUCTION_SELECTION, this->SelectionColor->text());
+  Settings::instance().setValue(MEDUSA_COLOR_INSTRUCTION_OPERATOR,  this->OperatorColor->text());
+  Settings::instance().setValue(MEDUSA_COLOR_INSTRUCTION_KEYWORD,   this->KeywordColor->text());
+  Settings::instance().setValue(MEDUSA_COLOR_INSTRUCTION_LABEL,     this->LabelColor->text());
+  Settings::instance().setValue(MEDUSA_COLOR_INSTRUCTION_STRING,    this->StringColor->text());
+  Settings::instance().setValue(MEDUSA_COLOR_ADDRESS_BACKGROUND,    this->AddressBackground->text());
 
   // Font
-  Settings::instance().setValue(MEDUSA_FONT_TEXT, this->textFont->font().toString());
+  Settings::instance().setValue(MEDUSA_FONT_TEXT, this->TextFont->font().toString());
 
   Settings::instance().saveChanges();
 
@@ -113,6 +120,6 @@ void SettingsDialog::setFont(void)
   if (ok)
   {
     curLine->setText(font.toString());
-    this->textFont->setFont(font);
+    this->TextFont->setFont(font);
   }
 }
