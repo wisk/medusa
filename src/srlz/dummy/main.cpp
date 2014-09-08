801d6b710795820639e82a6db025b90f50f9c4ec
wisk <szkudl.k@gmail.com>
2014-09-08 18:15:24 +0200

    srlz: these modules are deprecated, remove them

--- src/srlz/dummy/main.cpp
+++
@@ -1,5 +0,0 @@
-#include "dummy_serialize.hpp"
-
-int main(void) { return 0; }
-
-medusa::Serialize* GetSerialize(void) { return new DummySerialize; }
--- src/srlz/sqlite/main.cpp
+++
@@ -1,5 +0,0 @@
-#include "sqlite_serialize.hpp"
-
-int main(void) { return 0; }
-
-medusa::Serialize* GetSerialize(void) { return new SqliteSerialize; }
