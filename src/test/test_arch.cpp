b12f8c1c35e1ad1734ece6481cf694681ec53fb1
Kévin Szkudłapski <szkudl.k@gmail.com>
2014-09-09 22:05:39 +0200

    test: implement first test for arm architecture

--- src/test/test_arch.cpp
+++ src/test/test_arch.cpp
@@ -1,8 +1,10 @@
 #define BOOST_TEST_MODULE TestArchitecture
 #include <boost/test/unit_test.hpp> 
 
+#include <medusa/binary_stream.hpp>
 #include <medusa/module.hpp>
 #include <medusa/architecture.hpp>
+#include <medusa/instruction.hpp>
 
 BOOST_AUTO_TEST_SUITE(architecture_test_suite)
 
@@ -11,8 +13,23 @@ BOOST_AUTO_TEST_CASE(arch_arm_test_case)
   BOOST_MESSAGE("Testing ARM architecture");
 
   auto& rModMgr = medusa::ModuleManager::Instance();
-  auto pArchArm = rModMgr.LoadModule<medusa::TGetArchitecture>(".", "arm");
-  BOOST_CHECK(pArchArm != nullptr);
+  auto pArmGetter = rModMgr.LoadModule<medusa::TGetArchitecture>(".", "arm");
+  BOOST_CHECK(pArmGetter != nullptr);
+  auto pArmDisasm = pArmGetter();
+
+  auto const ArmMode = pArmDisasm->GetModeByName("arm");
+  auto const ThumbMode = pArmDisasm->GetModeByName("thumb");
+  BOOST_CHECK(ArmMode != 0);
+  BOOST_CHECK(ThumbMode != 0);
+
+  {
+    // e52de004        push    {lr}            ; (str lr, [sp, #-4]!)
+    medusa::MemoryBinaryStream MemBinStrm("\x04\xe0\x2d\xe5", 4);
+    medusa::Instruction Insn;
+    BOOST_CHECK(pArmDisasm->Disassemble(MemBinStrm, 0x0, Insn, 0));
+  }
+
+  delete pArmDisasm;
 }
 
 BOOST_AUTO_TEST_CASE(arch_avr8_test_case)
