5eb3fb4ca6510d458e4e60fffe1171ea26a05c35
Kévin Szkudłapski <szkudl.k@gmail.com>
2015-01-30 00:35:23 +0100

    bind_python: split bind_python/pydusa into two differents modules (to avoid renaming)

--- src/bind/python/pydusa.cpp
+++ src/bind/python/pydusa.cpp
@@ -1,5 +1,4 @@
 #include <boost/python.hpp>
-#include <mutex>
 
 #include "pydusa.hpp"
 
@@ -39,54 +38,3 @@ BOOST_PYTHON_MODULE(pydusa)
   PydusaDocument();
   PydusaMedusa();
 }
-
-MEDUSA_NAMESPACE_BEGIN
-
-std::once_flag g_PythonInit;
-
-BindingPython::BindingPython(void)
-{
-  std::call_once(g_PythonInit, [](){ Py_Initialize(); });
-}
-
-BindingPython::~BindingPython(void)
-{
-}
-
-std::string BindingPython::GetName(void) const
-{
-  return "python";
-}
-
-bool BindingPython::Bind(Medusa& rCore)
-{
-  if (!Execute("import pydusa"))
-    return false;
-
-  return true;
-}
-
-//ref: http://www.boost.org/doc/libs/1_57_0/libs/python/doc/tutorial/doc/html/python/embedding.html
-bool BindingPython::Execute(std::string const& rCode)
-{
-  auto MainModule = bp::import("__main__");
-  auto MainNamespace = MainModule.attr("__dict__");
-
-  try
-  {
-    auto Ignored = bp::exec(rCode.c_str(), MainNamespace);
-  }
-  catch (bp::error_already_set const&)
-  {
-    // TODO: remove this or intercept stderr and print message using Log::Write
-    PyErr_Print();
-    return false;
-  }
-  return true;
-}
-
-Binding* GetBinding(void) { return new BindingPython; }
-
-int main(void) { return 0; }
-
-MEDUSA_NAMESPACE_END
