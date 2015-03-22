cb202381facf4552b4d795be8b0ea58d28350019
Ivan Kulikov <gunmetal313@gmail.com>
2015-03-22 23:12:00 +0300

    dos_ldr: IsCompatible. add some code

--- src/ldr/dos/dos_loader.cpp
+++ src/ldr/dos/dos_loader.cpp
@@ -18,29 +18,31 @@ bool DosLoader::IsCompatible(BinaryStream const& rBinStrm) {
     if ((DosHdr.e_cparhdr * 16)<sizeof (DosHdr))
         return false;
 
-
-
-
-
-
+    if (DosHdr.e_crlc != 0) {
+        if (DosHdr.e_lfarlc + (DosHdr.e_crlc * 4) > DosHdr.e_cparhdr * 16 ||
+                (DosHdr.e_lfarlc && DosHdr.e_lfarlc < sizeof (DosHdr)) ||
+                !DosHdr.e_lfarlc) return false;
+    }
+    
+    
+    
     return true;
 }
 
 void DosLoader::Map(Document& rDoc, Architecture::VSPType const& rArchs) {
 
     BinaryStream const& rBinStrm = rDoc.GetBinaryStream();
-    
+
     DosHeader DosHdr;
-    
-    if (!rBinStrm.Read(0x0, &DosHdr, sizeof(DosHdr)))
-  {
-    Log::Write("ldr_pe") << "unable to read DOS header" << LogEnd;
-    return;
-  }
-  DosHdr.Swap(LittleEndian);
-  
-  
-  
+
+    if (!rBinStrm.Read(0x0, &DosHdr, sizeof (DosHdr))) {
+        Log::Write("ldr_pe") << "unable to read DOS header" << LogEnd;
+        return;
+    }
+    DosHdr.Swap(LittleEndian);
+
+
+
 
     rDoc.AddMemoryArea(new MappedMemoryArea(
             "raw",
